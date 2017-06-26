ff0f79209b56679f9a4ee6487b883f0723dbeab2
cen19 <wilsoncen@gmail.com>
2017-06-26 13:33:11 -0700

    Setting up compoenents

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-var webpack = require('webpack');
 var path = require('path');
+var SRC_DIR = path.join(__dirname, '/react-client/src');
+var DIST_DIR = path.join(__dirname, '/react-client/dist');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-
-var config = {
-  entry: APP_DIR + '/index.jsx',
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
   }
 };
-
-module.exports = config;
