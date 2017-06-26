ccf0988f97fac289decf9a7de37b8c1ba61c0c7a
Jeffrey <jdsun010@gmail.com>
2017-06-26 12:36:14 -0700

    feat add correct prefix, add serverside setup with dependencies

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use( bodyParser.urlencoded({ extended: true }) );
 // app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/', function(req, res) {
-  res.send('hello world')
+  res.send('hello world!')
 });
 
 
