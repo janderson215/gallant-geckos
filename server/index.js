0656bb356e96cbdca08d4bec4076f13682ec16d1
Jeffrey <jdsun010@gmail.com>
2017-06-26 12:34:20 -0700

    add express server setup & body parser, jquery dependencies

---
+++ server/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }) );
+
+
+// UNCOMMENT FOR REACT
+// app.use(express.static(__dirname + '/../react-client/dist'));
+
+app.get('/', function(req, res) {
+  res.send('hello world')
+});
+
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
+
