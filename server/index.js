a7b824c22655a516654f49c40b4fe836d8fd7614
Jon Anderson <janderson215@gmail.com>
2017-06-29 12:18:55 -0700

    Refactor anonymous Promise on server index | Fix dependencies on package.json

--- server/index.js
+++ server/index.js
@@ -5,13 +5,13 @@ const rp = require('request-promise-native');
 const googleAPIkey = require('../keys.js').googleAPIkey;
 
 const app = express();
+
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get('/', function(req, res) {
-  res.status(200).send('Hello World');
-});
+const nearbyRequestPrefix = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=';
+const iframePrefix = '<iframe src="https://www.google.com/maps/embed/v1/place?q=';
 
 let addresses = [];
 let geocodedAddresses = [];
@@ -21,16 +21,13 @@ let address;
 let coords;
 let latSum = 0;
 let lngSum = 0;
-
-const nearbyRequestPrefix = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=`
 let nearbyRequestSuffix;
-const iframePrefix = `<iframe src="//www.google.com/maps/embed/v1/place?q=`
-const photoUrlPrefix = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=`;
 
 app.post('/addresses', (req, res) => {
   let type = req.body.activity;
   let addresses = req.body.locations;
-  new Promise((resolve, reject) => {
+  
+  let geocodeAddresses = new Promise((resolve, reject) => {
     addresses.forEach(address => {
       address = address.split(' ').join('+');
       let geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${googleAPIkey}`;
@@ -43,8 +40,9 @@ app.post('/addresses', (req, res) => {
           }
       });
     });
-  })
-  .then(() => {
+  });
+
+  geocodeAddresses.then(() => {
     geocodedAddresses.forEach(geocodedAddress => {
       latSum += Number(geocodedAddress[0]);
       lngSum += Number(geocodedAddress[1]);
@@ -59,7 +57,7 @@ app.post('/addresses', (req, res) => {
       pointsOfInterest = JSON.parse(responseObject).results;
       pointsOfInterest.forEach(pointOfInterest => {
         let address = pointOfInterest.vicinity.split(' ').join('%20'); //url encoding
-        pointOfInterest.iframe = `<iframe src="//www.google.com/maps/embed/v1/place?q=${address}&zoom=17&key=${googleAPIkey}"></iframe>`
+        pointOfInterest.iframe = `${iframePrefix.concat(address)}&zoom=17&key=${googleAPIkey}"></iframe>`
       });
     })
     .then(() => {
@@ -73,14 +71,13 @@ app.post('/addresses', (req, res) => {
   });
 });
 
-
 app.get('/pointsOfInterest', (req, res) => {
   console.log(pointsOfInterest);
   res.status(200).send(pointsOfInterest);
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port " + app.get('port'), this.address().port, app.settings.env);
+  console.log("Express server listening on port " + this.address().port);
 });
 
 module.exports = app;
