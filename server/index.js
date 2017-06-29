a43fa397c33cce7aca21748a940df3c8ae74648c
Paul Dockery <paul.david.dockery@gmail.com>
2017-06-29 14:51:55 -0700

    modify key settings, keeping ignored but pointing to correct location

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const rp = require('request-promise-native');
-const googleAPIkey = require('../keys.js').googleAPIkey;
+const googleAPIkey = require('../react-client/src/key.js').googleAPIkey;
 
 const app = express();
 
