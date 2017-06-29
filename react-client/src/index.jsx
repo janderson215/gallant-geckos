c4a62f215a6e358bd591f15295c28ab8940d3ca1
cen19 <wilsoncen@gmail.com>
2017-06-29 10:07:18 -0700

    Able to use Iframe to load dummy data

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import LocationsEntrySet from './components/LocationsEntryForm.jsx';
-
-
+import Iframe from 'react-iframe';
+import dummy from './dummy-data.js';
 // import more components such as templates
 
 class App extends React.Component {
@@ -14,6 +14,10 @@ class App extends React.Component {
     };
   }
 
+  handleDummyData() {
+    let data = dummy[0].iframe.slice(12, (dummy[0].length - 10));
+    return data;
+  }
 
   handleSubmit (data) {
     console.log(`the client has submitted "${JSON.stringify(data)}"`);
@@ -41,6 +45,12 @@ class App extends React.Component {
       <div>
         <h1>Midpoint</h1>
         <LocationsEntrySet onSubmit={this.handleSubmit.bind(this)}/>
+        <Iframe url="http://www.google.com/maps/embed/v1/place?q=110%20Robinson%20Street,%20San%20Francisco&zoom=17&key=AIzaSyD7Hq8ejGKI9t3JIbnfz2myKOScIY5lnq0"
+          width="450px"
+          height="450px"
+          display="initial"
+          position="relative"
+          />
       </div>
     );
   }
