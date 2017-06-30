adb6bd6f79688cbc401b18cc8563c0df195fdff8
Paul Dockery <paul.david.dockery@gmail.com>
2017-06-30 16:42:13 -0700

    render resultslistentries

---
+++ react-client/src/components/ResultsListEntry.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../geosuggest.css';
+
+const ResultsListEntry = (props) => (
+  <div className="recommended-place">
+    <div className="info-left recommended-name"
+         onClick={() => props.handleSelectResult(props.recommendedPlace)}
+      >{props.recommendedPlace.name}
+    </div>
+    <div className="info-left recommended-address">{props.recommendedPlace.vicinity}</div>
+    <img className="media-right recommended-image" src={props.recommendedPlace.icon} />
+  </div>
+);
+
+export default ResultsListEntry;
