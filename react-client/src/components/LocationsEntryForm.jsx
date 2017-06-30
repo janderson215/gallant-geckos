9a99cf0b2ef24d38d71b566bdf51c2eaaf4a33e3
cen19 <wilsoncen@gmail.com>
2017-06-29 19:16:01 -0700

    (feature) buttons linked up and fields are added/removed properly

--- react-client/src/components/LocationsEntryForm.jsx
+++ react-client/src/components/LocationsEntryForm.jsx
@@ -9,17 +9,36 @@ class AddressSet extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: [],
-      count: 2,
+      addresses: [],
+      phoneNumbers: [],
+      count: 2, // starting number of fields
       activity: '',
     };
     this.handleAddressChange = this.handleAddressChange.bind(this);
+    this.handlePhoneNumberChange = this.handlePhoneNumberChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleActivityChange(event) {
+  handleActivityChange(e) {
     this.setState({
-      activity: event.target.value
+      activity: e.target.value
+    });
+  }
+
+  handleAddressChange(i, value) {
+    console.log(`i ${i} e ${value}`);
+    let addresses = this.state.addresses.slice();
+    addresses[i] = value;
+    this.setState({
+      addresses
+    });
+  }
+
+  handlePhoneNumberChange(i, e) {
+    let phoneNumbers = this.state.phoneNumbers.slice();
+    phoneNumbers[i] = e.target.value;
+    this.setState({
+      phoneNumbers
     });
   }
 
@@ -30,42 +49,40 @@ class AddressSet extends React.Component {
     });
   }
 
-  handleRemoveAddress(index) {
-    let locations = this.state.locations.slice();
-    console.log(`idx: ${index} value: ${locations[index]}`);
-    locations.splice(index, 1);
+  handleRemoveEntry(index) {
+    let addresses = this.state.addresses.slice();
+    let phoneNumbers = this.state.phoneNumbers.slice();
+    phoneNumbers.splice(index, 1);
+    // console.log(`idx: ${index} value: ${addresses[index]}`);
+    addresses.splice(index, 1);
     this.setState({
       count: this.state.count - 1,
-      locations
+      addresses,
+      phoneNumbers
+
     });
   }
 
-  handleAddressChange(i, value) {
-    console.log(`i ${i} e ${value}`);
-    let locations = this.state.locations.slice();
-    locations[i] = value;
-    this.setState({
-      locations
-    });
-  }
+
 
   handleSubmit(event) {
     // alert(`Hello, you submitted ${this.state.value}`);
     // need this line to below to pass the value from the input to the index file so that it can be used there
     event.preventDefault(); 
     var data = {
-      locations: this.state.locations,
-      activity: this.state.activity
+      activity: this.state.activity,
+      
+
     };
     this.props.onSubmit(data);
   }
 
   onSuggestSelect(i, suggest) {
     // console.log(`${i} ${suggest.label}`);
-    let locations = this.state.locations.slice();
-    locations[i] = suggest.label;
+    let addresses = this.state.addresses.slice();
+    addresses[i] = suggest.label;
     this.setState({
-      locations
+      addresses
     });
   }
 
@@ -76,21 +93,25 @@ class AddressSet extends React.Component {
         ref={el => this._geoSuggest = el}
         onChange={this.handleAddressChange.bind(this, i)}
         onSuggestSelect={this.onSuggestSelect.bind(this, i)}
-        initialValue={this.state.locations[i] || ''}
+        initialValue={this.state.addresses[i] || ''}
       />
     );
   }
 
   createPhoneNumberField(i) {
     return (
-      <input>
-      </input>
+      <input 
+        type="text" 
+        value={this.state.phoneNumbers[i] || ''}
+        placeholder={`Phone Number #${i + 1}`}
+        onChange={this.handlePhoneNumberChange.bind(this, i)}
+      />
     );
   }
 
   createRemoveFieldButton(i) {
     return (
-      <RaisedButton className="remove" label="Remove" onClick={this.handleRemoveAddress.bind(this, i)}/>
+      <RaisedButton className="remove" label="Remove" onClick={this.handleRemoveEntry.bind(this, i)}/>
     );
   }
 
@@ -101,12 +122,14 @@ class AddressSet extends React.Component {
         formItems.push(
           <li key={i}>
             {this.createGeosuggest(i)}
+            {this.createPhoneNumberField(i)}
           </li>
         );
       } else {
         formItems.push(
           <li key={i}>
             {this.createGeosuggest(i)}
+            {this.createPhoneNumberField(i)}
             {this.createRemoveFieldButton(i)}
           </li>
         );
