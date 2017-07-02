d1ca5a034b474f7b2f320b6ff57a8b8050322c5b
cen19 <wilsoncen@gmail.com>
2017-07-02 13:03:29 -0700

    (refactor) fields show up again but data not yet being gathered properly

--- react-client/src/components/PhoneNumberField.jsx
+++ react-client/src/components/PhoneNumberField.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Phone, {isValidPhoneNumber} from 'react-phone-number-input';
+import rrui from 'react-phone-number-input/rrui.css';
+import rpni from 'react-phone-number-input/style.css';
 
-const PhoneNumberField = () => (
+const PhoneNumberField = ({i, value, onChange}) => (
   <Phone
+    className="phone"
+    placeholder={`Phone Number #${i + 1}`}
+    onChange={onChange}
+    value={value}
+    country="US"
     />
 );
 
