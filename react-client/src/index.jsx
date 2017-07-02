e650d4e18e5b6adcd26389a221c342193dba3e17
cen19 <wilsoncen@gmail.com>
2017-07-01 19:30:57 -0700

    (refactor) beginning refactoring

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import LocationsEntrySet from './components/LocationsEntryForm.jsx';
+import EntrySet from './components/EntryForm.jsx';
 import Iframe from 'react-iframe';
 import dummy from './dummy-data.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -57,7 +57,7 @@ class App extends React.Component {
         <Paper zDepth={1}>
           <span>
             <AppBar title="Midpoint" showMenuIconButton={false} />
-              <LocationsEntrySet onSubmit={this.handleSubmit.bind(this)}/>
+              <EntrySet onSubmit={this.handleSubmit.bind(this)}/>
               <Iframe url={this.state.iframe}
                 width="450px"
                 height="450px"
